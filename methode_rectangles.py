8effd732830a92d50df4199cab9b085699b0a0fd
Adrien Huyghebaert <adrien.huyghebaert.1@ens.etsmtl.ca>
2024-06-08 19:41:15 -0400

    Suppression des fonctions inutiles dans "méthode rectangles"

--- methode_rectangles.py
+++ methode_rectangles.py
@@ -35,36 +35,3 @@ def methode_des_rectangles_numpy(a, b, pas, p_1, p_2, p_3, p_4, n):
     aire_totale = np.sum(pas * y)  # Calcul de l'aire totale
     return x, y, aire_totale
 
-
-# ================================================================================================
-# Affichage des courbes
-def afficher_courbes(liste_x_py, liste_y_py, x_np, y_np,  pas):
-    plt.bar(liste_x_py, liste_y_py, width=pas, align='edge', alpha=0.8, edgecolor='b')
-    plt.bar(liste_x_py, liste_y_py, width=pas, align='edge', alpha=0.3, edgecolor='g')
-    plt.xlabel('x')
-    plt.ylabel('f(x)')
-    plt.title("Méthode des rectangles et solution analytique 'Python' et 'NumPy'")
-    plt.show()
-
-
-# ================================================================================================
-# Fonction principale
-
-borne_a = 20
-borne_b = 40
-n = 10
-p1 = 10
-p2 = 4
-p3 = 1
-p4 = 50
-
-# Création du pas
-pas_resolution = (borne_b - borne_a) / n
-
-liste_x_python, liste_y_python, aire_totale_python =\
-    methode_des_rectangles_py(borne_a, pas_resolution, p1, p2, p3, p4, n)
-
-x_numpy, y_numpy, aire_totale_numpy = (
-    methode_des_rectangles_numpy(borne_a, borne_b, pas_resolution, p1, p2, p3, p4, n))
-
-afficher_courbes(liste_x_python, liste_y_python, x_numpy, y_numpy, pas_resolution)
