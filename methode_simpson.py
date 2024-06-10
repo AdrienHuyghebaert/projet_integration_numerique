de8a3d4a26d741958b8ce0aed7d14eeab5c081b5
Baptiste Rouanet <baptiste.rnet@gmail.com>
2024-06-10 00:49:00 -0400

    Version à réviser avant push final. Toutes les fonctionnalitées ont été ajoutées.

--- methode_simpson.py
+++ methode_simpson.py
@@ -10,18 +10,24 @@ import scipy.integrate as sci
 
 
 # Section calcul python simple
+
+# Fonction de calcul de l'aire sous la courbe entre les points a et b à l'aide de la fonction de Simspon
 def formule_simpson_simple(p1, p2, p3, p4, a, b):
     int_intervalle = ((b-a)/6) * (fction_simple(p1, p2, p3, p4, a) + 4*fction_simple(p1, p2, p3, p4, (a+b)/2) + fction_simple(p1, p2, p3, p4, b))
     return int_intervalle
 
 
+# Calcul des points de la courbe et la valeur de l'intégrale selon la méthode de Simpson
 def calcul_integrale_simpson_simple(p1, p2, p3, p4, a, b, n):
+    # Creation de l'interval de calcul
     interval = (b - a)/n
     pts_courbe = [fction_simple(p1, p2, p3, p4, a)]
     somme = 0
     x = a
     i = n
     pts_calcul = [a]
+
+    # calcul des points de la fonction associé aux valeurs de l'interval et de la somme des aires sous la courbe associé
     while i > 0:
         somme += formule_simpson_simple(p1, p2, p3, p4, x, x + interval)
         x += interval
@@ -31,6 +37,7 @@ def calcul_integrale_simpson_simple(p1, p2, p3, p4, a, b, n):
     return somme, pts_calcul, pts_courbe
 
 
+# Calcul de la fonction à intégrer
 def fction_simple(p1, p2, p3, p4, x):
     fct_eval = p1 + p2*x + p3*(x**2) + p4*(x**3)
     return fct_eval
@@ -38,18 +45,26 @@ def fction_simple(p1, p2, p3, p4, x):
 
 # Section calcul python vectorisé
 
+# Fonction de calcul de l'aire sous la courbe entre les points a et b à l'aide de la fonction de Simspon avec le module Numpy
 def formule_simpson_vect(p1, p2, p3, p4, a, b):
     int_intervalle = ((b-a)/6) * (fction_vect(p1, p2, p3, p4, a) + 4*fction_vect(p1, p2, p3, p4, (a+b)/2) + fction_vect(p1, p2, p3, p4, b))
     return int_intervalle
 
 
 def calcul_integrale_simpson_vect(p1, p2, p3, p4, a, b, n):
+    # définition de l'interval de calcul
     pts_calcul = np.linspace(a, b, n)
+
+    # calcul des points de la fonction associé aux valeurs de l'interval
     pts_courbe = fction_vect(p1, p2, p3, p4, pts_calcul)
+
+    # et de la somme des aires sous la courbe associé
     somme = np.sum(formule_simpson_vect(p1, p2, p3, p4, pts_calcul[0:n-1], pts_calcul[1:n]))
+
     return somme, pts_calcul, pts_courbe
 
 
+# Calcul de la fonction à intégrer à l'aide de Numpy
 def fction_vect(p1, p2, p3, p4, x):
     fct_eval = p1 + p2*x + p3*np.power(x,2) + p4*np.power(x, 3)
     return fct_eval
@@ -58,9 +73,16 @@ def fction_vect(p1, p2, p3, p4, x):
 # Section methode importée
 
 def calcul_integrale_simpson_scipy(p1, p2, p3, p4, a, b, n):
+    # définition de l'interval de calcul
     pts_calcul = np.linspace(a, b, n)
-    pts_courbe = p1 + p2*pts_calcul + p3*np.power(pts_calcul,2) + p4*np.power(pts_calcul, 3)
-    somme = sci.simpson(pts_courbe, pts_calcul)
+
+    # calcul des points de la fonction associé aux valeurs de l'interval
+    pts_courbe = p1 + p2*pts_calcul + p3*np.power(pts_calcul, 2) + p4*np.power(pts_calcul, 3)
+
+    # et de la somme des aires sous la courbe associé
+    # lien de la doc : https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.simpson.html
+    somme = sci.simpson(pts_courbe, x=pts_calcul)  # Mettre les valeurs de y de la courbe en premier argument et le x associé en deuxième argument lien de la doc : https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.simpson.html
+
     return somme
 
 
@@ -68,7 +90,45 @@ def calcul_integrale_simpson_scipy(p1, p2, p3, p4, a, b, n):
 
 def tracer_courbes(p1, p2, p3, p4, a, b, n):
     # On importe les résutlats des fonctions :
+    somme_simple, pts_calcul_simple, pts_courbe_simple = calcul_integrale_simpson_simple(p1, p2, p3, p4, a, b, n)
+    somme_vect, pts_calcul_vect, pts_courbe_vect = calcul_integrale_simpson_vect(p1, p2, p3, p4, a, b, n)
+    somme_scipy = calcul_integrale_simpson_scipy(p1, p2, p3, p4, a, b, n)
 
-    fig, axs = plt.subplots(1, 3)
-    fig.suptitle("Comparaison des résultats des méthodes Simpson")
-    axs[0, 0].plt.plot()
+    # Données de la courbe basique :
+    pts_calcul = np.linspace(a, b, n)
+    pts_courbe = p1 + p2 * pts_calcul + p3 * np.power(pts_calcul, 2) + p4 * np.power(pts_calcul, 3)
+    # On trace les graphiques associés
+    plt.figure(10)
+    # fig, axs = plt.subplots(1, 3)
+    plt.suptitle("Comparaison des résultats des méthodes Simpson")
+
+    # On trace le graph de la méthode simple
+    plt.subplot(121).set_title("Méthode simple")
+    plt.plot(pts_calcul_simple, pts_courbe_simple, label="Méthode simple", color='#27F94E')
+    plt.plot(pts_calcul, pts_courbe, label="Courbe primitive", color='#F53315')
+    plt.fill_between(pts_calcul_simple, pts_courbe_simple, color='#539ecd')
+    plt.legend()
+    plt.grid()
+
+    # On trace le graph de la méthode vectorisée
+    plt.subplot(122).set_title("Méthode vectorisée")
+    plt.plot(pts_calcul_vect, pts_courbe_vect, label="Méthode vectorisée", color='#27C6F9')
+    plt.plot(pts_calcul, pts_courbe, label="Courbe primitive", color='#F53315')
+    plt.fill_between(pts_calcul_vect, pts_courbe_vect, color='#539ecd')
+    plt.legend()
+    plt.grid()
+
+    plt.show()
+
+------------------------------------------------------------------------------------------------------------------------
+### Paramètres de test :
+
+# p1 = 24
+# p2 = -30
+# p3 = -50
+# p4 = 3
+# a = -1
+# b = 1
+# n = 10
+#
+# tracer_courbes(p1, p2, p3, p4, a, b, n)
